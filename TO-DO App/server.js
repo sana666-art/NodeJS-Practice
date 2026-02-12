a55b4d3080fe2101365e72e1594a99c7c0dbbec4
Sana Khalid <102246653+sana666-art@users.noreply.github.com>
2026-02-13 02:07:23 +0500

    update server.js

--- TO-DO App/server.js
+++ TO-DO App/server.js
@@ -32,7 +32,7 @@ app.use(methodOverride('_method'));
 
 // Session configuration
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({

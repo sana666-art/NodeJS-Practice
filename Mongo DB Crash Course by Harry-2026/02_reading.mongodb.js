70f4b6369526fe8f3fa1b5e1f9cbc6bce98b001f
Sana Khalid <102246653+sana666-art@users.noreply.github.com>
2026-02-14 03:18:00 +0500

    Create 02_reading.mongodb.js

---
+++ Mongo DB Crash Course by Harry-2026/02_reading.mongodb.js
@@ -0,0 +1,20 @@
+use('ecommerce')
+
+// db.products.find();
+
+// db.products.find().pretty()
+
+// db.products.find({ category: "Electronics" })
+
+
+// db.products.find({ price: { $gt: 1000 } });
+
+// db.products.find({ $or: [{ category: "Electronics" }, { stock: { $lt: 50 } }] })
+
+// db.products.find({ $and: [{ category: "Electronics" }, { stock: { $lt: 50 } }] })
+
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
+
+// db.products.find().sort({ price:-1 }).limit(2)
+
+db.products.find().sort({ price:-1 }).skip(1).limit(2)
